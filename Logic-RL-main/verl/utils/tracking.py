ac675c102f56d85af127da2833e4e2aced714bcf
Oliver van Erven <142365241+Ovanerven@users.noreply.github.com>
2025-05-18 20:29:06 +0200

    Update tracking.py

--- Logic-RL-main/verl/utils/tracking.py
+++ Logic-RL-main/verl/utils/tracking.py
@@ -40,7 +40,7 @@ class Tracking(object):
             import wandb
             import os
             WANDB_API_KEY = os.environ.get("WANDB_API_KEY", None)
-            WANDB_API_KEY = '28ef37a0b698bcefcfd9217e2e7b9695360d533c'
+            WANDB_API_KEY = 'd7c4c0d5cea2d3c3143bea851765b771b96e76e8'
             if WANDB_API_KEY:
                 wandb.login(key=WANDB_API_KEY)
             wandb.init(project=project_name, name=experiment_name, config=config)
