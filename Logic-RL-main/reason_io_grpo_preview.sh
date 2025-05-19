5cc53fbdf9f54a3d773f44c48b3636792c3051e4
Oliver van Erven <142365241+Ovanerven@users.noreply.github.com>
2025-05-19 22:00:59 +0200

    update preview params

--- Logic-RL-main/reason_io_grpo_preview.sh
+++ Logic-RL-main/reason_io_grpo_preview.sh
@@ -9,15 +9,15 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=reinforce_plus_plus \
     data.train_files=data/reason_io/reason_io_dataset_preview.parquet \
     data.val_files=data/reason_io/reason_io_dataset_val.parquet \
-    data.train_batch_size=1 \
-    data.val_batch_size=1 \
+    data.train_batch_size=8 \
+    data.val_batch_size=8 \
     data.max_prompt_length=10000 \
     data.max_response_length=4096 \
     actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.actor.optim.lr=4e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=1 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=1 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=16 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -30,7 +30,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=2 \
-    actor_rollout_ref.rollout.temperature=0.7 \
+    actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.ref.log_prob_micro_batch_size=160 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
@@ -38,7 +38,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.logger=['wandb'] \
     trainer.project_name='ReinforcePP_ReasonIO' \
     trainer.experiment_name="Qwen-3B_ReasonIO_${timestamp}" \
-    trainer.n_gpus_per_node=1 \
+    trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.default_local_dir=actor_checkpoints/reason_io_${timestamp} \
     trainer.default_hdfs_dir=null \
