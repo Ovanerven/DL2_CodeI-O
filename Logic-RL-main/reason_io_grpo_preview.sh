7aa1a39efdea2a3cbb80785712235c4339e41a8b
Oliver van Erven <142365241+Ovanerven@users.noreply.github.com>
2025-05-19 20:24:21 +0200

    gpus 1 for preview

--- Logic-RL-main/reason_io_grpo_preview.sh
+++ Logic-RL-main/reason_io_grpo_preview.sh
@@ -38,7 +38,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.logger=['wandb'] \
     trainer.project_name='ReinforcePP_ReasonIO' \
     trainer.experiment_name="Qwen-3B_ReasonIO_${timestamp}" \
-    trainer.n_gpus_per_node=2 \
+    trainer.n_gpus_per_node=1 \
     trainer.nnodes=1 \
     trainer.default_local_dir=actor_checkpoints/reason_io_${timestamp} \
     trainer.default_hdfs_dir=null \
