6c8f05ca0849acf37661d920c663556c78f442a5
Ovanerven <142365241+Ovanerven@users.noreply.github.com>
2025-05-24 11:39:43 +0200

    Update reason_io_grpo_7b.sh

--- Logic-RL-main/reason_io_grpo_7b.sh
+++ Logic-RL-main/reason_io_grpo_7b.sh
@@ -28,7 +28,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size=160 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.temperature=0.7 \
     actor_rollout_ref.ref.log_prob_micro_batch_size=160 \
