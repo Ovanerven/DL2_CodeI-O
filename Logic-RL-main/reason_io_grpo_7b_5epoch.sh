9e0c8fdef924aa825d85164a08ec82facae27be9
Ovanerven <142365241+Ovanerven@users.noreply.github.com>
2025-05-25 01:01:38 +0200

    5 epoch jobs

--- Logic-RL-main/reason_io_grpo_7b_5epoch.sh
+++ Logic-RL-main/reason_io_grpo_7b_5epoch.sh
@@ -1,5 +1,13 @@
 set -x
 
+# Environment variables
+export VLLM_ATTENTION_BACKEND=XFORMERS
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export MKL_THREADING_LAYER=GNU
+export TORCH_CUDA_ALLOW_TF32=1
+export HYDRA_FULL_ERROR=1
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
 MODEL_PATH=Qwen/Qwen2.5-7B-Instruct
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
@@ -9,14 +17,14 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=reinforce_plus_plus \
     data.train_files=data/reason_io/reason_io_dataset_train.parquet \
     data.val_files=data/reason_io/reason_io_dataset_val.parquet \
-    data.train_batch_size=4 \
+    data.train_batch_size=8 \
     data.val_batch_size=8 \
     data.max_prompt_length=4096 \
     data.max_response_length=4096 \
     actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.actor.optim.lr=4e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=4 \
     actor_rollout_ref.actor.ppo_micro_batch_size=4 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
@@ -28,7 +36,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size=160 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.temperature=0.7 \
     actor_rollout_ref.ref.log_prob_micro_batch_size=160 \
@@ -37,11 +45,11 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['wandb'] \
     trainer.project_name='ReasonIO' \
-    trainer.experiment_name="Qwen-7B_ReasonIO_${timestamp}" \
+    trainer.experiment_name="Qwen-7B-OG_ReasonIO_${timestamp}" \
     trainer.n_gpus_per_node=4 \
     trainer.nnodes=1 \
     trainer.default_local_dir=actor_checkpoints/reason_io_${timestamp} \
     trainer.default_hdfs_dir=null \
     trainer.save_freq=420 \
     trainer.test_freq=10 \
-    trainer.total_epochs=1 $@ 2>&1 | tee logs/reasonio/reason_io_${timestamp}.log
+    trainer.total_epochs=5 $@ 2>&1 | tee logs/reasonio/reason_io_${timestamp}.log
