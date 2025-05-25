1157d61bed294e5742b1cc9b61a0561d0d30849b
Ovanerven <142365241+Ovanerven@users.noreply.github.com>
2025-05-25 08:52:09 +0200

    increase micro, mini batch size. increase max_response length

--- Logic-RL-main/reason_io_grpo_7b_5epoch.sh
+++ Logic-RL-main/reason_io_grpo_7b_5epoch.sh
@@ -20,12 +20,12 @@ python3 -m verl.trainer.main_ppo \
     data.train_batch_size=8 \
     data.val_batch_size=8 \
     data.max_prompt_length=4096 \
-    data.max_response_length=4096 \
+    data.max_response_length=8192 \
     actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.actor.optim.lr=4e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=4 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=4 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=64 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
